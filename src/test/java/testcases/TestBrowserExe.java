3ae72de984a9a8d3bf8b259a12abc0295504070a
RenzoAbstracta <83373510+RenzoAbstracta@users.noreply.github.com>
2023-04-07 09:14:45 -0300

    TestBrowserExe experimentando varios browsers

--- src/test/java/testcases/TestBrowserExe.java
+++ src/test/java/testcases/TestBrowserExe.java
@@ -2,8 +2,14 @@ package testcases;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,10 +19,25 @@ import org.testng.annotations.Test;
 public class TestBrowserExe {
 
     private WebDriver driver;
+    public static String browser = "chrome";
+
 
     @BeforeClass
     public void setUp() {
-        driver = new ChromeDriver();
+
+        if (browser.equals("chrome")) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+			driver = new ChromeDriver();
+		} else if (browser.equals("firefox")) {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setBrowserVersion("0.30.0");
+			driver = new FirefoxDriver();
+		} else if (browser.equals("edge")) {
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+			driver = new EdgeDriver();
+		}
     }
 
     @Test
